4a4259f6511ee239d248bb0d14005b293f1e46c2
AliexBabskiy <ababskiy@gmail.com>
2023-12-01 02:48:45 +0300

    первый вариант

---
+++ Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cl1
+{
+    internal class Menu
+    {
+        private int minStrelochka;
+        private int maxStrelochka;
+
+        public Menu(int min, int max)
+        {
+            minStrelochka = min;
+            maxStrelochka = max;
+        }
+        public static int Show(int minStrelochka, int maxStrelochka)
+        {
+            int pos = 1;
+            ConsoleKeyInfo info;
+            Console.CursorVisible = false;
+            do
+            {
+                Console.SetCursorPosition(0, pos);
+                Console.WriteLine("->");
+
+                info = Console.ReadKey();
+
+                Console.SetCursorPosition(0, pos);
+                Console.WriteLine("  ");
+
+                if (info.Key == ConsoleKey.UpArrow && pos != minStrelochka)
+                {
+                    pos--;
+                }
+                else if (info.Key == ConsoleKey.DownArrow && pos != maxStrelochka)
+                {
+                    pos++;
+                }
+                else if (info.Key == ConsoleKey.Escape)
+                {
+                    return -1;
+                }
+                //return post;
+            }
+            while (info.Key != ConsoleKey.Enter);
+            return pos;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime;
+using Cl1;
+
+class Provodnic
+{
+    static string userName = Environment.UserName;      //определение имени устройства
+    static void Main()
+    {
+
+        //ShowPapka("C:\\");
+        Diski();
+    }
+    static void Diski()
+    {
+        while (true)
+        {
+            Console.Clear();
+            //Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in allDrives)
+            {
+                Console.Write($"  {drive.Name} {drive.AvailableFreeSpace / (1024 * 1024 * 1024):N2} ГБ свободно из {drive.TotalSize / (1024 * 1024 * 1024):N2} ГБ");
+                Console.WriteLine();
+            }
+
+            int pos = Menu.Show(0, allDrives.Length - 1);
+            if (pos == -1)
+            {
+                return;
+            }
+            ShowPapka($"{allDrives[pos]}");
+        }
+    }
+    static void ShowPapka(string p)
+    {
+        while (true)
+        {
+                                                                                   //var directory = new DirectoryInfo(p);
+            Console.Clear();
+            string[] paths = Directory.GetDirectories(p);                          //DirectoryInfo[] paths = directory.GetDirectories(p);
+            string[] pathFiles = Directory.GetFiles(p);                                 //FileInfo[] pathFiles = directory.GetFiles(p);
+
+            foreach (string path in paths)                                         //foreach (DirectoryInfo path in paths)
+            {
+                Console.WriteLine("  " + path);           //Console.WriteLine($"  {path.FullName} {path.CreationTime}");
+            }
+            
+            foreach (string path1 in pathFiles)                                  //foreach (FileInfo path1 in pathFiles)
+            {
+                Console.WriteLine("  " + path1);                         //Console.WriteLine("  " + path1.FullName);
+            }
+
+            int poz = paths.Length + pathFiles.Length;
+
+            int pos = Menu.Show(0, poz - 1);
+            if (pos == -1 )
+            {
+                return;
+            }
+            if (pos < paths.Length)
+            {
+                ShowPapka(paths[pos]);
+            }
+            else
+            {
+                Process.Start(pathFiles[pos - paths.Length]);
+                //ShowPapka(pathFiles[pos]);
+            }
+            //ShowPapka(paths[pos]);
+        }
+    }
+}
